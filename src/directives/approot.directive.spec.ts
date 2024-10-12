bb7a3b0fe11867c4396e9543de8e23efd7f921bf
HongEngVathana <engvathanahong@gmail.com>
2024-10-12 09:27:47 +0700

    add

---
+++ src/directives/approot.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ApprootDirective } from './approot.directive';
+
+describe('ApprootDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ApprootDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/directives/approot.directive.ts
@@ -0,0 +1,9 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appApproot]',
+  standalone: true,
+})
+export class ApprootDirective {
+  constructor() {}
+}
